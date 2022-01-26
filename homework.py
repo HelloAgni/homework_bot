ac37c8de906e0da54254cfbf46d62809c8853a33
Байбак Кирилл <zloifox@gmail.com>
2022-01-26 18:46:27 +0300

    tokens

--- homework.py
+++ homework.py
@@ -1,11 +1,16 @@
-...
+import os
+
+import requests
+from telegram.ext import CommandHandler, Updater
+from telegram import ReplyKeyboardMarkup
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,10 +25,14 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
+    # Отправляет сообщение в Телеграм чат (TELEGRAM_CHAT_ID)
+    # Принимает на вход два параметра экземпляр класса Bot и строка с текстом сообщения
     ...
 
 
 def get_api_answer(current_timestamp):
+    # Запрос к единственному эндпоинту API
+    # В случае успешного запроса венуть ответ API и преобразовать json()
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
 
@@ -31,11 +40,16 @@ def get_api_answer(current_timestamp):
 
 
 def check_response(response):
-
+    # Проверка ответа API на корректность.
+    # В качестве параметра ответ API приведенный к типам данных Python
+    # ключ 'homeworks'
     ...
 
 
 def parse_status(homework):
+    # Извлекает из информации статус конкретной домашней работы
+    # Получает только один элемент из списка домашних работ
+    # В случае успеха возвращает в Телеграм строку из словаря 'HOMEWORK_STATUSES'
     homework_name = ...
     homework_status = ...
 
@@ -49,14 +63,19 @@ def parse_status(homework):
 
 
 def check_tokens():
+    # Проверка доступности переменных окружения
+    # Если отсутсвует хотябы одна переменная вернуть False 
     ...
 
 
 def main():
     """Основная логика работы бота."""
-
+    # Сделать запрос API
+    # Проверить ответ
+    # Если есть обновления получить статус работы из обновления
+    # и отправить в Телеграм
+    # Подождать время и сделать новый запрос
     ...
-
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
 
