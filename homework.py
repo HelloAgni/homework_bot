5f2161a0eef60a21d8545ba28e4f795af0150c06
Байбак Кирилл <zloifox@gmail.com>
2022-02-05 17:05:20 +0300

    25 min

--- homework.py
+++ homework.py
@@ -15,7 +15,8 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-TELEGRAM_RETRY_TIME = 600
+# TELEGRAM_RETRY_TIME = 600
+TELEGRAM_RETRY_TIME = 1500
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
